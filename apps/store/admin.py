9a88b44b3c047d8cd3c0b30c70c6298f46e02baa
yueyoum <yueyoum@gmail.com>
2014-08-20 17:19:01 +0800

    store admin

--- apps/store/admin.py
+++ apps/store/admin.py
@@ -58,7 +58,7 @@ class StoreBuyLogAdmin(admin.ModelAdmin):
         'item_tp', 'Item', 'buyer', 'amount', 'buy_at',
     )
 
-    list_filter = ('item_tp', 'item_id', 'buyer',)
+    list_filter = ('tag', 'sell_type', 'item_tp', 'item_id', 'buyer',)
 
     def Item(self, obj):
         return ADMIN_ITEM[obj.item_tp][obj.item_id]
