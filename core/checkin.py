03c923dcc81a257f233fe406508190c41cbb95eb
yueyoum <yueyoum@gmail.com>
2014-09-21 11:10:02 +0800

    Fix get_checkin_obj

--- core/checkin.py
+++ core/checkin.py
@@ -3,13 +3,15 @@
 __author__ = 'Wang Chao'
 __date__ = '14-6-12'
 
+from django.conf import settings
 
 import arrow
 from apps.checkin.models import CheckInDate
 
 
 def get_checkin_obj():
-    checkins = CheckInDate.objects.filter(checkin_date__gte=arrow.utcnow().date())[0:1]
+    now_date = arrow.utcnow().to(settings.TIME_ZONE).date()
+    checkins = CheckInDate.objects.order_by('-checkin_date').filter(checkin_date__lte=now_date)[0:1]
     if checkins:
         checkin_obj = checkins[0]
     else:
