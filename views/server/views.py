208d1b3633323f1d75622b978b14bcb64bb0e38d
yueyoum <yueyoum@gmail.com>
2014-12-26 13:58:57 +0800

    nginx conf

--- views/server/views.py
+++ views/server/views.py
@@ -71,7 +71,7 @@ def server_list(request):
     else:
         account_id = None
 
-    is_test = request.environ['NEW_VERSION'] == '1'
+    is_test = request.environ['NEW_GAME_VERSION'] == '1'
     all_servers = get_server_list(account_id=account_id, is_test=is_test)
 
     top = None
